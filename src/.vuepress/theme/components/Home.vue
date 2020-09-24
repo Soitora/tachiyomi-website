d9d8dd8d502e26953aa190ea88f0264ed1efd900
Soitora <simon.mattila@protonmail.com>
2020-09-24 19:14:35 +0200

    Change font variables

--- src/.vuepress/theme/components/Home.vue
+++ src/.vuepress/theme/components/Home.vue
@@ -133,7 +133,7 @@ export default {
 				color #fff
 				cursor pointer
 				display inline-block
-				font-family $buttonFontFamily
+				font-family $fontCode
 				font-size 1.125rem
 				margin 0.25rem
 				padding 0.8rem
